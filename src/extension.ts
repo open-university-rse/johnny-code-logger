8fdfa8db264d75636a5fbee9ae089a53f317d9f7
Paul Lunn <paulilunn@gmail.com>
2021-02-19 19:42:42 +0000

    Now sends paste data

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,13 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+const USER_ID = 3;
+const BASE_REST_URL = 'http://0.0.0.0:8000';
+const CLIPBOARD_URL = BASE_REST_URL + "/api/clipboard/";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -22,6 +29,19 @@ export function activate(context: vscode.ExtensionContext) {
 		// console.log(clipboardContent);
 	
 		// send paste to server
+		// set content-type header and data as json in args parameter
+		var date = new Date();
+		var args = {
+			data: { user_id: USER_ID, time: date, text:clipboardContent },
+			headers: { "Content-Type": "application/json" }
+		};
+
+		client.post(CLIPBOARD_URL, args, function (data:any, response:any) {
+			// parsed response body as js object
+			console.log(data);
+			// raw response
+			console.log(response);
+		});
 	
 		//dispose of the overridden editor.action.clipboardCopyAction- back to default copy behavior
 		clipboardPasteDisposable.dispose();
@@ -36,15 +56,6 @@ export function activate(context: vscode.ExtensionContext) {
 			disposableArray.push(clipboardPasteDisposable);
 		}); 
 	}
-
-	// disposableArray.push(vscode.commands.registerCommand('clipboard.paste', () => {
-	// 	console.log("clipboard.paste");
-    //     vscode.commands.executeCommand("editor.action.clipboardPasteAction");
-    // }));
-
-	// disposableArray.push(vscode.commands.registerCommand('clipboard.pasteFromClipboard', () => {
-    //     console.log("clipboard.pasteFromClipboard");
-    // }));
 
 	context.subscriptions.concat(disposableArray);
 }
